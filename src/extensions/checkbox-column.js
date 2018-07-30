c143e41bcba7fbb0bcd220459b5b800d5ff36209
Panit Wechasil <panitw@hotmail.com>
2018-07-30 22:29:55 +0700

    Allow checkbox to sync back

--- src/extensions/checkbox-column.js
+++ src/extensions/checkbox-column.js
@@ -15,8 +15,19 @@ export class CheckboxColumnExtension {
             if (this._grid.model.canEdit(e.rowIndex, e.colIndex)) {
                 e.cellContent.style.pointerEvents = 'all';
             }
+
+            let checkbox = e.cellContent.firstChild;
+            checkbox.addEventListener('change', (checkboxEvent) => {
+                this._grid.model.setDataAt(e.rowIndex, e.colIndex, checkboxEvent.target.checked);
+            });
         } else {
             e.cellContent.innerHTML = e.data;
+        }
+    }
+
+    cellUpdate (e) {
+        if (e.cellContent.firstChild) {
+            e.cellContent.firstChild.checked = e.data;
         }
     }
 
